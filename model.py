56683d6eb5b6cbf0a064006932c79e87bc37abd7
Uruskhan Bokov <bokovuruskhan@gmail.com>
2022-05-27 17:58:33 +0300

    add Project, Service models

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import backref
+
 from config import MyApp
 
 database = MyApp.database
@@ -6,3 +8,29 @@ database = MyApp.database
 class Object(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String(255), nullable=False)
+    projects = database.relationship('Project',
+                                     backref=database.backref('projects', lazy=True))
+
+
+class Project(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    name = database.Column(database.String(255), nullable=False)
+    object_id = database.Column(database.Integer, database.ForeignKey('object.id'), nullable=False)
+    services = database.relationship("Service", secondary="ProjectServices")
+
+
+class Service(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    name = database.Column(database.String(255), nullable=False)
+    price = database.Column(database.REAL, nullable=False)
+    margin = database.Column(database.REAL, nullable=False, default=0.0)
+    projects = database.relationship("Project", secondary="ProjectServices")
+
+
+class ProjectServices(database.Model):
+    __tablename__ = "ProjectServices"
+    id = database.Column(database.Integer, primary_key=True)
+    project_id = database.Column(database.Integer, database.ForeignKey('project.id'), nullable=False)
+    service_id = database.Column(database.Integer, database.ForeignKey('service.id'), nullable=False)
+    project = database.relationship(Project, backref=backref("ProjectServices", cascade="all, delete-orphan"))
+    service = database.relationship(Service, backref=backref("ProjectServices", cascade="all, delete-orphan"))
