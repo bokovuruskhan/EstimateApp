da346a8b91fa5dfac56d2ee4e8ea10855dbaab4e
Uruskhan Bokov <bokovuruskhan@gmail.com>
2022-05-31 17:00:46 +0300

    add import services from csv

--- admin.py
+++ admin.py
@@ -1,6 +1,10 @@
+import os
+from io import TextIOWrapper
+
+from flask import request, flash
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
-from werkzeug.utils import redirect
+from werkzeug.utils import redirect, secure_filename
 
 from model import *
 
@@ -8,8 +12,31 @@ app = MyApp.app
 database = MyApp.database
 
 
-@app.route("/data/import")
+def get_file_path(filename):
+    return os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+
+def read_services_from_csv(filename):
+    import csv
+    with open(get_file_path(filename), encoding="UTF-8") as csvfile:
+        reader = csv.reader(csvfile, delimiter=';', quotechar='|')
+        for row in reader:
+            if database.session.query(Service).filter(Service.name == row[0]).first() is None:
+                database.session.add(Service(name=row[0], price=int(row[1]), margin=float(row[2])))
+        database.session.commit()
+
+
+@app.route("/data/import", methods=['GET', 'POST'])
 def import_data():
+    if request.method == 'POST':
+        if "file" not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files.get("file")
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(get_file_path(filename))
+            read_services_from_csv(filename)
     return redirect("/admin")
 
 
